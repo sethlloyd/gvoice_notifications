6508741d17791c1aa553d80321467821403ccba5
Seth Lloyd <sethwlloyd@gmail.com>
2009-08-09 04:41:38 -0700

    First Upload

---
+++ gv_notify_0_012.py
@@ -0,0 +1,147 @@
+#! /usr/bin/env python
+
+from googlevoice import Voice
+import xmpp
+import urllib
+from BeautifulSoup import BeautifulSoup
+import time
+import getpass
+import smtplib
+
+
+
+
+
+
+voice = Voice()
+g_user = raw_input("Complete google voice login name: ")
+g_passwd = getpass.getpass("Google voice password: ")
+voice.login(g_user, g_passwd)
+
+
+EMAIL_NOTI = False
+
+# determine if user wants im or email notification
+if raw_input("Enter 0 for email notification, anything else for IM: ")=='0':
+    EMAIL_NOTI = True
+    
+
+# The following assumes that the account to send the message from is a gmail.com based account
+# Obviously, if this is not true, the parameters would need to be changed
+
+if EMAIL_NOTI:
+    email={}
+    email['Server'] = "smtp.gmail.com"
+    email['From'] = g_user
+    email['To'] = "%s" % raw_input("Email address to send to: ")
+    
+else:
+    im_username = raw_input("Jabber account user name to send message from (for gmail do not include @gmail.com): ")
+    im_passwd = getpass.getpass("Jabber account password: ")
+    im_sendto = raw_input("Account to send message to (for gmail, INCLUDE @gmail.com): ")
+    client = xmpp.Client('gmail.com',debug=[])
+    client.connect(server=('talk.google.com',5223))
+    client.auth(im_username,im_passwd, 'botty')
+    client.sendInitPresence()
+
+# Set
+
+first = True
+sms_count = {}
+
+# repeat the following loop until the program is killed
+while True:
+    # first find out how many unread messages there are
+#    allmsg= voice.inbox()['unreadCounts']
+#    new_sms = voice.inbox()['unreadCounts']['sms']
+
+    allsms = voice.sms()
+    new_sms = allsms['unreadCounts']['sms']
+#    print allsms
+    smss = allsms['messages']
+    found = 0
+
+
+    # iterate through unread sms messages, keeping track of what notifications have been sent.
+    # it's a bit tricky to keep track of what's really new since
+    # an unread sms thread may contain more than 1 new message since the last iteration, though google
+    # voice only reports one unread message (i.e. just that there are new messages in that thread)
+    # thus for each thread you have, this keeps track of how many messages there were at
+    # the previous iteration and notifies you of all new messages since
+    
+    for itms in reversed(smss.keys()):
+
+        # the first time we want to build the list of message totals
+        if first:
+            found=-1
+            
+        if found == new_sms:
+            break
+
+      #  print found
+      #  print new_sms
+        smsID=smss[itms]['id']
+        sms_parse=BeautifulSoup(voice.sms_html())
+
+
+        # BeautifulSoup very nicely parses the html tags to pull out the actual text messages
+        sms_content = sms_parse.find(id=smsID).findAll(attrs={"class":"gc-message-sms-text"})
+        sms_from = sms_parse.find(id=smsID).findAll(attrs={"class":"gc-message-sms-from"})
+        # here's the actual number of texts you've received from the specific user
+        clen=len(sms_content)
+
+        # check if we already have an entry for that sender, and compare with the number of previous
+        # messages received to see how many are actually new.  The first time the script runs, it
+        # will only send you notification of the last received sms for any sms messages marked as unread
+        
+        if smsID in sms_count:
+            unread_sms=clen-sms_count[smsID]            
+        else:
+            if first:              
+                unread_sms=1
+            else:
+                unread_sms=clen
+                
+
+        # store the unread count for comparison next time
+        sms_count[smsID]=clen
+
+        # build up the notification message containing all new sms messages
+        if smss[itms]['isRead']== False:
+    #        print sms_parse.find(id=smsID)
+            print smss[itms]
+            found=found+1
+       #     print found
+            if unread_sms > 0:
+                if EMAIL_NOTI:
+                    email['Subject'] = "New sms from " + sms_from[0].string
+                    email['Text']=""
+                    for txt in sms_content[clen-unread_sms:clen]:
+        #                print txt.PreviousSibling.string
+                         #   continue
+                        email['Text'] = email['Text'] + txt.string + '\r\n'
+                        
+                    server = smtplib.SMTP(email['Server'], 587)
+                    server.ehlo()
+                    server.starttls()
+                    server.ehlo()
+                    server.login(g_user, g_passwd)
+                    server.sendmail(g_user,email['To'],("From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n%s" % (email['From'],email['To'],email['Subject'],email['Text'])))
+                    server.quit()
+
+
+                else:
+                    im="New sms from " + smss[itms]['displayNumber'] + ': '                
+                    for txt in sms_content[clen-unread_sms:clen]:
+                        im=im + txt.string + '\r\n'
+
+                    message= xmpp.Message(im_sendto,im)
+                    message.setAttr('type','chat')
+                    client.send(message)
+
+            
+
+    first= False
+
+    #sleep for 60 seconds before repeating.
+    time.sleep(60)
